091923afb9ec269ebc10b64ee5d073f35702707b
Mike Wickwar <mike.wickwar@gmail.com>
2016-02-08 13:39:39 -0700

    seeds and migrations ran both local and on heroku deploying heroku now

--- migrations/20160208132419_create_authors.js
+++ migrations/20160208132419_create_authors.js
@@ -1,8 +1,13 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('authors', function(table) {
+    table.increments();
+    table.string('first_name');
+    table.string('last_name');
+    table.text('bio');
+    table.string('portrait_url');
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('authors');
 };
