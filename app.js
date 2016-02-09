b6ca80ecfcfb10e1e874b7704b2ceb6c64171810
Mike Wickwar <mike.wickwar@gmail.com>
2016-02-08 23:09:27 -0700

    heroku shenanigans fixed?

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 // var knex = require('./db/knex');
 
 var routes = require('./routes/index');
-// var books = require('./routes/books');
+var books = require('./routes/books');
 var authors = require('./routes/authors');
 
 var app = express();
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-// app.use('/books', books);
+app.use('/books', books);
 app.use('/authors', authors);
 
 // catch 404 and forward to error handler
