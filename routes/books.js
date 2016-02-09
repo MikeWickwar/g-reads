e890c56c1f80c38ae76f29476e9073256c1196b8
Mike Wickwar <mike.wickwar@gmail.com>
2016-02-08 21:19:11 -0700

    heroku is going to work

--- routes/books.js
+++ routes/books.js
@@ -9,13 +9,16 @@ function Authors() {
   return knex('authors')
 }
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  Books().fullOuterJoin('authbook_junction', 'books.id', 'authbook_junction.id').then(function (books) {
-    Authors().fullOuterJoin('authbook_junction', 'authors.id', 'authbook_junction.author_id').then(function (authors) {
-    console.log(books+'***********BOOKS');
-    res.render('books/index', {title: 'I got youre book title right here!', books: books, authors: authors})
-    })
-  })
-});
+// router.get('/', function(req, res, next) {
+//   Books().fullOuterJoin('authbook_junction', 'books.id', 'authbook_junction.id').then(function (books) {
+//     Authors().fullOuterJoin('authbook_junction', 'authors.id', 'authbook_junction.author_id').then(function (authors) {
+//     console.log(books+'***********BOOKS');
+//     res.render('books/index', {title: 'I got youre book title right here!', books: books, authors: authors})
+//     })
+//   })
+// });
 
+router.get('/', function (req, res, next) {
+    res.send('yo mama')
+})
 module.exports = router;
