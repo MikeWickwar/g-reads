bf538327e6f8ca1791c121eac726d2543cc25242
Mike Wickwar <mike.wickwar@gmail.com>
2016-02-09 14:35:52 -0700

    updateing junction table and books table from new book form

--- routes/books.js
+++ routes/books.js
@@ -8,6 +8,9 @@ function Books() {
 function Authors() {
   return knex('authors')
 }
+function Authbooks() {
+  return knex('authbook_junction')
+}
 /* GET home page. */
  router.get('/', function(req, res, next) {
    Books().fullOuterJoin('authbook_junction', 'books.id', 'authbook_junction.id').then(function (books) {
@@ -17,6 +20,26 @@ function Authors() {
      })
    })
  });
+
+ router.post('/', function (req, res, next) {
+   var booksEntry = {
+     title: req.body.title,
+     genre: req.body.genre,
+     description: req.body.description,
+     cover_url: req.body.cover_url
+   }
+   Books().select().insert(booksEntry).returning('id').then(function(results){
+     var authEntry = {
+       author_id: req.body.author_id,
+       book_id: results[0]
+     }
+    console.log("********");
+     console.log(results[0]);
+     Authbooks().select().insert(authEntry).then(
+       res.send('check yo db. you postit')
+      )
+    })
+  })
 
  router.get('/new', function (req, res, next) {
    Authors().select().then(function (authors) {
