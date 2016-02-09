37acca77c19147e6da279cbd7829839274a92521
Mike Wickwar <mike.wickwar@gmail.com>
2016-02-09 15:53:44 -0700

    working edit page updates the db

--- routes/books.js
+++ routes/books.js
@@ -32,12 +32,12 @@ function Authbooks() {
      var authEntry = {
        author_id: req.body.author_id,
        book_id: results[0]
-     }  
+       }
      Authbooks().select().insert(authEntry).then(
        res.send('check yo db. you postit')
-      )
-    })
-  })
+       )
+     })
+   })
 
  router.get('/new', function (req, res, next) {
    Authors().select().then(function (authors) {
@@ -45,10 +45,43 @@ function Authbooks() {
    })
  })
 
+
  router.get('/:id', function (req, res, next) {
    Books().where('books.id', req.params.id).then(function (books) {
       Authors().fullOuterJoin('authbook_junction', 'authors.id', 'authbook_junction.author_id').then(function (authors) {
      res.render('books/index', {title: 'individual book still on index view', books: books, authors: authors})
+    })
+  })
+})
+
+router.get('/:id/edit', function (req, res, next) {
+  Books().where('books.id', req.params.id).first().then(function (book) {
+    Authors().join('authbook_junction', 'authors.id', 'authbook_junction.author_id').then(function (authors) {
+      console.log(book.id);
+      console.log('*******');
+      console.log(authors);
+      res.render('books/edit', {book: book, authors: authors})
+    })
+  })
+})
+
+router.post('/:id/edit', function (req, res, next) {
+  var booksEntry = {
+    title: req.body.title,
+    genre: req.body.genre,
+    description: req.body.description,
+    cover_url: req.body.cover_url
+  }
+  var authEntry = {
+    author_id: req.body.author_id,
+    book_id: req.params.id
+    }
+  Books().where('books.id', req.params.id).first().update(booksEntry).then(function () {
+    Authbooks().where('book_id', req.params.id).update(authEntry).then(function () {
+      // console.log(book.id);
+      // console.log('*');
+      // console.log(authors);
+      res.redirect('/books/'+req.params.id)
     })
   })
 })
