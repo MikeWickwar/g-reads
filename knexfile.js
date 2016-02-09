093b878d1dec1e4da0c6d54d51dec861f89fcb53
Mike Wickwar <mike.wickwar@gmail.com>
2016-02-09 09:32:38 -0700

    testing heroku issue

--- knexfile.js
+++ knexfile.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL + '?ssl=true',
+    connection: process.env.DATABASE_URL + '?ssl=true'
     // seeds: {
     //   directory: './seeds'
     // }
