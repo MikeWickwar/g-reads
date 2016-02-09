40f8b36d14df481a9484f9ee5426c7eb7516f6f7
Mike Wickwar <mike.wickwar@gmail.com>
2016-02-08 19:40:41 -0700

    working books page, can display books with their authors, not styled yet

--- routes/books.js
+++ routes/books.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('../db/knex');
 
+function Books() {
+  return knex('books')
+}
+function Authors() {
+  return knex('authors')
+}
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('books/index', {title: 'I got youre book title right here!'})
+  Books().fullOuterJoin('authbook_junction', 'books.id', 'authbook_junction.id').then(function (books) {
+    Authors().fullOuterJoin('authbook_junction', 'authors.id', 'authbook_junction.author_id').then(function (authors) {
+    console.log(books+'***********BOOKS');
+    res.render('books/index', {title: 'I got youre book title right here!', books: books, authors: authors})
+    })
+  })
 });
 
 module.exports = router;
