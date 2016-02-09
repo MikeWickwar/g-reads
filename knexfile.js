c82ce97675afd1e8a48ced9a17fcaafe4641403f
Mike Wickwar <mike.wickwar@gmail.com>
2016-02-08 22:42:26 -0700

    heroku shenanigans

--- knexfile.js
+++ knexfile.js
@@ -11,6 +11,9 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL + '?ssl=true'
+    connection: process.env.DATABASE_URL + '?ssl=true',
+    seeds: {
+      directory: './seeds'
+    }
   }
 };
