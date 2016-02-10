03e43965639ecb87aa2f0b9255b8f9092ee2d13a
Mike Wickwar <mike.wickwar@gmail.com>
2016-02-09 23:46:21 -0700

    a user can see all of the authors

--- routes/authors.js
+++ routes/authors.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('../db/knex');
 
+function Books() {
+  return knex('books')
+}
+function Authors() {
+  return knex('authors')
+}
+function Authbooks() {
+  return knex('authbook_junction')
+}
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('authors/index', { title: 'AUTHORS' });
+  Authors().select().then(function (authors) {
+    Books().fullOuterJoin('authbook_junction', 'books.id', 'authbook_junction.book_id').then(function (books) {
+      res.render('authors/index', { title: 'AUTHORS', authors: authors, books: books });
+    })
+  })
 });
 
 module.exports = router;
