818aa05a52521a38a87f88e3a604925e79ace23f
Mike Wickwar <mike.wickwar@gmail.com>
2016-02-10 17:01:34 -0700

    working validations for new and editing book

--- routes/books.js
+++ routes/books.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var knex = require('../db/knex');
+var validate = require('../lib/validations')
 
 function Books() {
   return knex('books')
@@ -28,15 +29,22 @@ router.post('/', function (req, res, next) {
     description: req.body.description,
     cover_url: req.body.cover_url
   }
-  Books().select().insert(booksEntry).returning('id').then(function(results){
-    var authEntry = {
-      author_id: req.body.author_id,
-      book_id: results[0]
-     }
-    Authbooks().select().insert(authEntry).then(
-     res.redirect('/books')
-     )
-   })
+  var errors = validate(booksEntry)
+  if (errors.length) {
+    Authors().select().then(function (authors) {
+      res.render('books/new', {errors: errors, authors: authors})
+    })
+  }else{
+    Books().select().insert(booksEntry).returning('id').then(function(results){
+      var authEntry = {
+        author_id: req.body.author_id,
+        book_id: results[0]
+       }
+      Authbooks().select().insert(authEntry).then(
+       res.redirect('/books')
+       )
+     })
+    }
  })
 
 router.get('/new', function (req, res, next) {
@@ -71,10 +79,18 @@ router.post('/:id/edit', function (req, res, next) {
     description: req.body.description,
     cover_url: req.body.cover_url
   }
+  var errors = validate(booksEntry)
   var authEntry = {
     author_id: req.body.author_id,
     book_id: req.params.id
     }
+  if (errors.length) {
+    Books().where('books.id', req.params.id).first().then(function (book) {
+      Authors().select().then(function (authors) {
+        res.render("books/edit", {book:book, authors: authors, errors: errors })
+      })
+    })
+  }
   Books().where('books.id', req.params.id).first().update(booksEntry).then(function () {
     Authbooks().where('book_id', req.params.id).update(authEntry).then(function () {
       res.redirect('/books/'+req.params.id)
